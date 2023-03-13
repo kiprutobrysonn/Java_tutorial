75419688b46f55840a41ce050a0a12b5574fc5a1
kiprutobrysonn <108465881+brycekip46@users.noreply.github.com>
2023-03-13 16:38:11 +0300

    Update Demo2.java

--- java_tuto/Demo2.java
+++ java_tuto/Demo2.java
@@ -3,6 +3,13 @@ package java_tuto;
 import javax.swing.JFrame;
 
 public class Demo2 extends JFrame{
+
+public Demo2(){
+this.setTitle("First Window");
+this.setSize(400,400);
+this.setBackground(Color.black);
+this setVisible 
+}
 	
 	public static void main(String []args) {
 		Demo2 d1 = new Demo2();
